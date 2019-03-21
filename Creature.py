32792a65fabb5c43ed5e4f8e20bc2ab689783ca8
Clark Lindsay <cmlind@umich.edu>
2019-03-21 18:06:51 -0400

    changed Creature init to be more idiomatic

--- Creature.py
+++ Creature.py
@@ -1,17 +1,14 @@
-class Creature:
-    def __init__(self, name, challengeRating, terrains=["Land"], creatureType="Beast"):
-        self.name = name
-        self.challengeRating = challengeRating
+import json
 
+class Creature:
+    def __init__(self, creatureJSON):
+        self.__dict__ = json.loads(creatureJSON)
         terrainError = ValueError('A creatures terrains must be one of: "Land", "Water", or "Sky"')
-        if not terrains:
+        if not self.terrains:
             raise terrainError
-        for terrain in terrains:
+        for terrain in self.terrains:
             if terrain != 'Land' or terrain != 'Water' or terrain != 'Sky':
                 raise terrainError
-                
-        self.terrains = terrains
-        self.creatureType = creatureType
     
     def __repr__(self):
         return 'Name: {}, CR: {}, Terrains: {}, Type: {}'.format(self.name, self.challengeRating, self.terrains, self.creatureType)
