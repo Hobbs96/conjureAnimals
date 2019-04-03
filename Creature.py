7d7c34479115893fd418bcd86ab6dc1010d23aa0
Clark <clarklindsay96@gmail.com>
2019-04-02 20:52:24 -0400

    small QOL changes

--- Creature.py
+++ Creature.py
@@ -5,7 +5,8 @@ class Creature:
         self.name = name
         self.challengeRating = challengeRating
 
-        terrainError = ValueError('A creatures terrains must be one of: "Land", "Water", or "Sky"')
+        terrainError = ValueError('All of a creatures terrains must be one of: "Land", "Water", or "Sky",' +
+        'and it must have at least one')
         if not terrains:
             raise terrainError
         for terrain in terrains:
@@ -16,7 +17,7 @@ class Creature:
         self.creatureType = creatureType
     
     def __repr__(self):
-        return 'Name: {}, CR: {}, Terrains: {}, Type: {}'.format(self.name, self.challengeRating, self.terrains, self.creatureType)
+        return '(Name: {}, CR: {}, Terrains: {}, Type: {})'.format(self.name, self.challengeRating, self.terrains, self.creatureType)
 
 #invoke as creature = Creature(json.loads(jsonObject, object_hook = asCreature))
 def asCreature(dictionary):
